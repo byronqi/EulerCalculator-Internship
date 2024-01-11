372590dc7bdc31b0ec4852f3afa6bfb822521e84
eshingji <eshji123@gmail.com>
2024-01-11 13:46:02 -0600

    finished boundary check for staggered

--- euler.cpp
+++ euler.cpp
@@ -50,35 +50,61 @@ class euler
             bool return_value = true;
             if (pattern == TRIANGULAR || pattern == SQUARE45) //boundary check: staggered
             {
-                //checking for a/b vs. k1 graph
+                //checking for a/b vs. k1 graph:
                 float a_b = a/b;
-                if(Re < 100)
+                if(Re < 100) //assume it's just 1
                 {
-                    //if a/b > 1.2 it's out of bounds
-                    if (a_b > 1.2)
+                    //if a/b > 1.25 and Re < 100 it's out of bounds b/c no data for this region
+                    if (a_b > 1.25 || a_b <= 0.5) //not sure of lower bound for 100
                     {
                         return_value = false;
                     }
                 }
-                if(Re >= 100 && Re < 1000)
+                if(Re >= 100 && Re <= 1000)
                 {
-
+                    if (a_b <= 0.5) //lower bound for 1000 unsure of lower bound for 100
+                    {
+                        return_value = false;
+                    }
+                    if (a_b > 1.2 && a_b < 1.25) //both undefined
+                    {
+                        return_value = false;
+                    }
+                    if (a_b >= 3.5) //upper for both
+                    {
+                        return_value = false;
+                    }
                 }
-                if(Re >= 1000 && Re < 10000)
+                if(Re > 1000 && Re <= 1000000) //1000 is undefined for 1.2-1.25- still calculate for 1000 & interpolate
                 {
-
+                    if (a_b <= 0.45 || a_b >= 3.5)
+                    {
+                        return_value = false;
+                    }
                 }
-                if(Re >= 10000 && Re < 100000)
+                if(Re > 1000000) // we don't have over 1E6.
                 {
-
+                    return_value = false;
                 }
-                if(Re >= 100000 && Re < 1000000)
-                {
 
+                //checking for Re vs Eu/k1 graph
+                if(a <= 2 && a >= 1.25)
+                {
+                    if(Re < 2)
+                    {
+                        return_value = false;
+                    }
                 }
-                if(Re >= 1000000)
+                if(a <= 2.5 && a > 2)
                 {
-
+                    if(Re < 7)
+                    {
+                        return_value = false;
+                    }
+                }
+                if(a > 2.5) // we don't have under 2.
+                {
+                    return_value = false;
                 }
             }
             else //boundary check: square
