1f0f574b7fa36cd5b0507f442f07b64007637eaf
CHEMCAD <eshing.ji@kinkaid.org>
2024-01-12 12:58:31 -0600

    fixed k1 staggered

--- euler.cpp
+++ euler.cpp
@@ -13,8 +13,14 @@ cEulerNumber::cEulerNumber(int x, float y, float z)
 
 float cEulerNumber::eulerNumberCalculation(float Re){
     // TODO: finish this and return Euler number
-    float Eu = 1.0;
-    return Eu;
+    if (pattern == TRIANGULAR || pattern == SQUARE45) //calculate for staggered.
+    {
+
+    }
+    else if (pattern == SQUARE || pattern == TRIANG60) //calculate for square.
+    {
+
+    }
 }
 
 // TODO: write checkBoundary() desc
@@ -22,14 +28,14 @@ bool cEulerNumber::checkBoundary(float Re)
 {
     float checkBoundary_A = calculate_a();
     float checkBoundary_B = calculate_b();
+    bool returnValue;
     if (pattern == TRIANGULAR || pattern == SQUARE45){
-        checkStaggeredBoundary(checkBoundary_A, checkBoundary_B, Re);
+        returnValue = checkStaggeredBoundary(checkBoundary_A, checkBoundary_B, Re);
     }
     if (pattern == SQUARE || pattern == TRIANG60){
-        checkSquareBoundary(checkBoundary_A, checkBoundary_B, Re);
+        returnValue = checkSquareBoundary(checkBoundary_A, checkBoundary_B, Re);
     }
-    // TODO: return a value
-    return false; // TEMP REMOVE WHEN COMPLETE
+    return returnValue;
 }
 
 float cEulerNumber::calculate_a()
@@ -82,7 +88,7 @@ bool cEulerNumber::checkStaggeredBoundary(float a, float b, float Re){
     if (pattern == TRIANGULAR || pattern == SQUARE45) //boundary check: staggered
     {
         //checking for a/b vs. k1 graph:
-        if(Re < 100) //assume it's just 1
+        if(Re < 100)
         {
             //if a/b > 1.25 and Re < 100 it's out of bounds b/c no data for this region
             if (abValue > 1.25 || abValue <= 0.5) //not sure of lower bound for 100
@@ -132,7 +138,7 @@ bool cEulerNumber::checkStaggeredBoundary(float a, float b, float Re){
             return_value = false;
         }
 
-        if(a > 2000000)
+        if(Re > 2000000)
         {
             return_value = false;
         }
@@ -171,59 +177,35 @@ float cEulerNumber::k1Square(float a, float b, float Re) {
 float cEulerNumber::k1Staggered(float a, float b, float Re)
 {
     float a_b = a/b;
+    float Re_values[5] = {100, 1000, 10000, 100000, 1000000};
+    float k1_values[5] = {0,0,0,0,0}; //not sure if I have to do this.
+    if (a_b <= 1.25)
+    {
+        k1_values[0] = 1;
+        k1_values[1] = pow(a_b, -0.048f); //from eqn 53
+    }
+    else
+    {
+        k1_values[0] = 0.93f*pow(a_b, 0.48); //from eqn 56
+        k1_values[1] = 0.951f*pow(a_b, 0.284f); //from eqn 57
+    }
+    k1_values[2] = 1.28 - 0.708f/a_b + 0.55/pow(a_b, 2) - 0.113f/pow(a_b, 3); //eqn 54
+    k1_values[3] = 2.016f - 1.675f*a_b + 0.948f*pow(a_b, 2) - 0.234f*pow(a_b, 3) + 0.021f*pow(a_b, 4); //eqn 55
+    k1_values[4] = 2.016f - 1.675f*a_b + 0.948f*pow(a_b, 2) - 0.234f*pow(a_b, 3) + 0.021f*pow(a_b, 4); //eqn 55
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (k1_values[i] < 1)
+        {
+            k1_values[i] = 1;
+        }
+    }
     float k1;
-    float k1_1;
-    float k1_2;
-    if (Re < 100)
-    {
-        k1 = 1;
-        return k1;
-    }
-    else if (Re < 1000)
-    {
-        if (a_b <= 1.25)
-        {
-            k1_1 = 1;
-            k1_2 = pow(a_b, -0.048f); //from eqn 53
-            if (k1_2 < 1)
-            {
-                k1_2 = 1;
-            }
-        }
-        else
-        {
-            k1_1 = 0.93f*pow(a_b, 0.48); //from eqn 56
-            k1_2 = 0.951f*pow(a_b, 0.284f); //from eqn 57
-        }
-    }
-    else if (Re < 10000)
-    {
-        if (a_b < 1.25)
-        {
-            k1_1 = pow(a_b, -0.048f); //from eqn 53
-            if (k1_1 < 1)
-            {
-                k1_1 = 1;
-            }
-        }
-        else
-        {
-            k1_1 = 0.951f*pow(a_b, 0.284f); //from eqn 57
-        }
-        k1_2 = 1.28 - 0.708/a_b + 0.55/pow(a_b, 2) - 0.113/pow(a_b, 3); //eqn 54
-    }
-    else if (Re < 100000)
-    {
-        k1_1 = 1.28 - 0.708f/a_b + 0.55/pow(a_b, 2) - 0.113f/pow(a_b, 3); //eqn 54
-        k1_2 = 2.016f - 1.675f*a_b + 0.948f*pow(a_b, 2) - 0.234f*pow(a_b, 3) + 0.021f*pow(a_b, 4);
-    }
-    else if (Re < 1000000)
-    {
-        k1 = 2.016f - 1.675f*a_b + 0.948f*pow(a_b, 2) - 0.234f*pow(a_b, 3) + 0.021f*(a_b, 4);
-        return k1;
-    }
-    //TODO: linear interpolation w/ k1_1 & k1_2
-    k1 = -1; //for now
+    float *x;
+    x = &Re_values[0];
+    float *y;
+    y = &k1_values[0];
+    //slin(Re, 5, x, y, &k1)
     return k1;
 }
 
